8aae14041085cc1b83db028d9214b8f5f8e52d72
Tommiee <tomdepom1999@gmail.com>
2017-12-20 13:57:15 +0100

    20-12-17 updates

---
+++ Assets/Scripts/Energy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is for the draining energy bar of igniculius
+public class Energy : MonoBehaviour {
+
+    Vector3 startSize;
+    Vector3 startPos;
+    float posChange;
+    float scaleChange;
+    bool move;
+    public float moveSpeed;
+
+	void Start () {
+        startSize = transform.localScale;
+        startPos = transform.position;
+        move = false;
+        posChange = -moveSpeed * transform.parent.transform.localScale.x; 
+        scaleChange = -0.08f;
+    }   
+	
+	void Update () {
+        posChange = -moveSpeed * transform.parent.transform.localScale.x;
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            move = !move;
+        }
+
+        if (transform.localScale.x <= 0)
+        {
+            move = false;
+        }
+
+        if(move)
+        {
+            transform.position = new Vector3(transform.position.x + (posChange * Time.deltaTime), transform.position.y, transform.position.z);
+            transform.localScale = new Vector3(transform.localScale.x + (scaleChange * Time.deltaTime), transform.localScale.y, transform.localScale.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = startPos;
+            transform.localScale = startSize;
+        }
+    }
+}
---
+++ Assets/Scripts/temp_Camera_Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temp_Camera_Movement : MonoBehaviour {
+    public float move;
+
+    //Quick and dirty script to test parallax scrolling
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position = new Vector3(this.transform.position.x + move,this.transform.position.y,this.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position = new Vector3(this.transform.position.x - move, this.transform.position.y, this.transform.position.z);
+        }
+	}
+}
